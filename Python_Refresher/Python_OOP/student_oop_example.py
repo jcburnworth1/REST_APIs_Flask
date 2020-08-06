6e582b797c50523d0fd7cebb7f348c4e971f1de3
J.C. Burnworth <jc.burnworth@S05010-MBPR.attlocal.net>
2020-08-05 23:30:30 -0500

    Reset file structure & add OOP example

---
+++ Python_Refresher/Python_OOP/student_oop_example.py
@@ -0,0 +1,15 @@
+##### Basic OOP Example #####
+## Setup Student Class ##
+class Student:
+    def __init__(self, name: str, year: int, grades: list): ## This is a method
+        self.name = name
+        self.year = year
+        self.grades = grades
+        self.student_average = sum(self.grades) / len(self.grades)
+
+    # def student_average(self): ## Utilize this method to calculate average
+    #     return sum(self.grades) / len(self.grades)
+
+
+## Create an instance of the class Student
+student = Student('J.C.', 7, [70,75,80,89,95])
