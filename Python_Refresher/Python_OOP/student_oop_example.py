863298ab85707b07edfe2eab1a8c2b58b317a47b
J.C. Burnworth <jc.burnworth@S05010-MBPR.attlocal.net>
2020-08-06 23:26:08 -0500

    __str__ and __repr__ examples

--- Python_Refresher/Python_OOP/student_oop_example.py
+++ Python_Refresher/Python_OOP/student_oop_example.py
@@ -15,3 +15,32 @@ class Student:
 student = Student('J.C.', 7, [70,75,80,89,95])
 
 #### __str__ & __repr__ #####
+## Person Class
+class Person:
+
+    def __init__(self, name, age):
+        """
+        Initialize Peron with the following parameters
+        :param name: Name of the individual
+        :param age: Age of the individual
+        """
+        self.name = name
+        self.age = age
+
+    def __str__(self): ## This will print to console when print(Person) called for readability
+        """
+        String representation of a Person object
+        :return: Person <name>, <age> years old.
+        """
+        return f"Person {self.name}, {self.age} years old."
+
+    def __repr__(self):
+        """
+        :return: <Person <name>, <age> years old.>
+        """
+        return f"<Person {self.name}, {self.age} years old.>"
+
+## Bob Object
+bob = Person('Bob', 35)
+
+print(bob) ## Prints the default <__main__.Person object at 0x10dc8d850>
